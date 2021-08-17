f80aeb3258203f58e4292fe15208c1378eee8a42
Furkan <furkan.turkal@trendyol.com>
2021-08-17 13:45:50 +0300

    initialize command funcs
    Signed-off-by: Furkan <furkan.turkal@trendyol.com>

--- cmd/list.go
+++ cmd/list.go
@@ -21,23 +21,31 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// listCmd represents the list command
-var listCmd = &cobra.Command{
-	Use:   "list",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
+// NewCmdList represents the list command
+func NewCmdList() *cobra.Command {
+	var namespace string
+
+	var cmd = &cobra.Command{
+		Use:   "list",
+		Short: "A brief description of your command",
+		Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
 
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("list called")
-	},
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("list called")
+		},
+	}
+
+	cmd.PersistentFlags().StringVarP(&namespace, "namespace", "n", "", "Target namespace")
+
+	return cmd
 }
 
 func init() {
-	rootCmd.AddCommand(listCmd)
+	rootCmd.AddCommand(NewCmdList())
 
 	// Here you will define your flags and configuration settings.
 
--- cmd/run.go
+++ cmd/run.go
@@ -21,23 +21,36 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// runCmd represents the run command
-var runCmd = &cobra.Command{
-	Use:   "run",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
+// NewCmdRun represents the run command
+func NewCmdRun() *cobra.Command {
+	var k8sVersion, apiServerPort, namespace, kubeconfig string
+
+	var cmd = &cobra.Command{
+		Use:   "run",
+		Short: "A brief description of your command",
+		Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
 
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("run called")
-	},
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("run called")
+		},
+	}
+
+	cmd.PersistentFlags().StringVarP(&k8sVersion, "kubernetes-version", "k", "", "Desired version of Kubernetes")
+	cmd.PersistentFlags().StringVarP(&apiServerPort, "api-server-port", "p", "", "API Server Port")
+	cmd.PersistentFlags().StringVarP(&namespace, "namespace", "n", "", "Target namespace")
+	cmd.PersistentFlags().StringVarP(&kubeconfig, "kubeconfig", "c", "", "Path to KUBECONFIG")
+
+
+
+	return cmd
 }
 
 func init() {
-	rootCmd.AddCommand(runCmd)
+	rootCmd.AddCommand(NewCmdRun())
 
 	// Here you will define your flags and configuration settings.
 
