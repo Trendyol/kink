98882d5b8d2e5d0ab065dfd26bae3fe23b2aaf5c
Furkan <furkan.turkal@trendyol.com>
2021-09-01 15:33:28 +0300

    fix(cmd): should not error out twice
    Fixes #3
    
    Signed-off-by: Furkan <furkan.turkal@trendyol.com>

--- cmd/root.go
+++ cmd/root.go
@@ -17,6 +17,7 @@ package cmd
 
 import (
 	"github.com/spf13/cobra"
+	"os"
 )
 
 var cfgFile string
@@ -34,5 +35,7 @@ var rootCmd = &cobra.Command{
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-	cobra.CheckErr(rootCmd.Execute())
+	if err := rootCmd.Execute(); err != nil {
+		os.Exit(1)
+	}
 }
